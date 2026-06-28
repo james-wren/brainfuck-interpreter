5e465e7286c38f86a668c556168b1f737706744e
tagtribble <tagtribble@gmail.com>
2026-06-27 23:43:53 -0400

    That was way easier than I expected lol, 50m total on wakatime, not including research, so now Ima build file reading then write docs.

---
+++ src/interpreter.asm
@@ -0,0 +1,140 @@
+section .data
+    code db "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.", 0
+
+section .bss
+    tape resb 30000
+
+section .text
+    global _start
+
+_start:
+    mov rsi, code
+    mov rbx, tape
+
+interpret_loop:
+    mov al, [rsi]
+    cmp al, 0
+    je exit
+
+    cmp al, '>'
+    je move_right
+    cmp al, '<'
+    je move_left
+    cmp al, '+'
+    je increment_byte
+    cmp al, '-'
+    je decrement_byte
+    cmp al, '.'
+    je output_byte
+    cmp al, ','
+    je input_byte
+    cmp al, '['
+    je loop_start
+    cmp al, ']'
+    je loop_end
+
+    jmp next_char
+
+; Pointer munipulation
+move_right:
+    inc rbx
+    jmp next_char
+
+move_left:
+    dec rbx
+    jmp next_char
+
+; Byte munipulation
+increment_byte:
+    inc byte [rbx]
+    jmp next_char
+
+decrement_byte:
+    dec byte [rbx]
+    jmp next_char
+
+; I/O operations
+output_byte:
+    push rsi
+    push rbx
+
+    mov rax, 1 ; sys_write
+    mov rdi, 1 ; stdout
+    mov rsi, rbx
+    mov rdx, 1
+    syscall
+
+    pop rbx
+    pop rsi
+    jmp next_char
+
+input_byte:
+    push rsi
+    push rbx
+
+    mov rax, 0 ; sys_read
+    mov rdi, 0 ; stdin
+    mov rsi, rbx
+    mov rdx, 1
+    syscall
+
+    pop rbx
+    pop rsi
+    jmp next_char
+
+loop_start:
+    cmp byte [rbx], 0
+    jne next_char
+
+    mov rcx, 1
+.skip_forward:
+    inc rsi
+    mov al, [rsi]
+
+    cmp al, '['
+    je .nest_in
+    cmp al, ']'
+    je .nest_out
+    jmp .skip_forward
+
+.nest_in:
+    inc rcx
+    jmp .skip_forward
+
+.nest_out:
+    dec rcx
+    cmp rcx, 0
+    jne .skip_forward
+    jmp next_char
+
+loop_end:
+    cmp byte [rbx], 0
+    je next_char
+
+    mov rcx, 1
+.skip_backward:
+    dec rsi
+    mov al, [rsi]
+
+    cmp al, ']'
+    je .nest_in_back
+    cmp al, '['
+    je .nest_out_back
+    jmp .skip_backward
+.nest_in_back:
+    inc rcx
+    jmp .skip_backward
+.nest_out_back:
+    dec rcx
+    cmp rcx, 0
+    jne .skip_backward
+    jmp next_char
+
+next_char:
+    inc rsi
+    jmp interpret_loop
+
+exit:
+    mov rax, 60
+    mov rdi, 0
+    syscall
